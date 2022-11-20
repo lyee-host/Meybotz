a68b501771afb3eaf96ecefd5fcb2c283410116a
lyee-host <lossgaming01@gmail.com>
2022-11-20 23:49:42 +0700

    Meybotz

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Create By MEIXGAN.
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '9d30efca66',
+}
+
+// Other
+global.owner = ['6281334915899','6283845185968','61279086875']
+global.ownernomer = ['6281334915899','6283845185968','61279086875']
+global.premium = ['6281334915899','6283845185968','61279086875']
+global.packname = 'MEYBotz'
+global.author = 'MARGA 19'
+global.sessionName = 'MEIXGAN'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Harap Tunggu ygy :v...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+global.thumb = fs.readFileSync('./image/Meixgan.jpg')
+global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
